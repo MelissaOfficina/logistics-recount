eb8cb9655c9c709f7fdfa1e5f885091d01c35fea
alla <dxmpsycho@gmail.com>
2025-09-22 18:06:08 +0500

    day 3

---
+++ src/components/tables/ShipmentsTable.vue
@@ -0,0 +1,34 @@
+<script setup lang="ts">
+defineProps(['shipments']);
+
+const formatDate = (date: string) => {
+  return new Date(date).toLocaleDateString('ru-RU', {
+    year: 'numeric',
+    month: 'long',
+    day: 'numeric'
+  });
+};
+</script>
+
+<template>
+  <table>
+    <thead>
+    <tr>
+      <th>ID</th>
+      <th>Поставщик</th>
+      <th>Дата прихода</th>
+      <th>Статус</th>
+      <th>Действие</th>
+    </tr>
+    </thead>
+    <tbody>
+    <tr v-for="shipment in shipments" :key="shipment.id" :data-testid="shipment.id">
+      <td>{{shipment.id}}</td>
+      <td>{{shipment.vendor}}</td>
+      <td>{{formatDate(shipment.date)}}</td>
+      <td>{{shipment.status}}</td>
+      <td><button>Открыть</button></td>
+    </tr>
+    </tbody>
+  </table>
+</template>
